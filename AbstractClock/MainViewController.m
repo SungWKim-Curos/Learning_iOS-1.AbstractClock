5472eaaac4fa414adbca70d75af29f631ebebc47
sungwkim <sungwkim@curos.co.kr>
2013-02-23 01:42:14 +0900

    10개의 정사각형 이미지들를 보여준다.

--- AbstractClock/MainViewController.m
+++ AbstractClock/MainViewController.m
@@ -8,16 +8,43 @@
 
 #import "MainViewController.h"
 
+static const int iSHAPE_ARRAY_SIZE = 10 ;
+
+static inline int RAND_INT( int iMin, int iMax )
+{ return iMin + arc4random()%( (iMax+1)-iMin ) ; }
+
+
+
 @interface MainViewController ()
 
 @end
 
 @implementation MainViewController
+{
+    NSMutableArray* _oShapeVwArr ;
+}
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    const CGAffineTransform scaleTransf = CGAffineTransformMakeScale( 0.1, 0.1 ) ;
+    _oShapeVwArr = [ [NSMutableArray alloc] initWithCapacity:iSHAPE_ARRAY_SIZE ] ;
+    
+    for( int i=0 ; i<iSHAPE_ARRAY_SIZE ; ++i )
+    {
+        NSString* oImgName = [ NSString stringWithFormat:@"PatchSquare%d.png", i ] ;
+        UIImage* oImg = [ UIImage imageNamed:oImgName ] ;
+        UIImageView* oImgVw = [ [UIImageView alloc] initWithImage:oImg ] ;
+        
+        oImgVw.transform = scaleTransf ;
+        oImgVw.center = CGPointMake( RAND_INT(0,320), RAND_INT(0,480) ) ;
+        
+        [ _oShapeVwArr addObject:oImgVw ] ;
+        [ super.view addSubview:oImgVw ] ;
+    }
 }
 
 - (void)didReceiveMemoryWarning
